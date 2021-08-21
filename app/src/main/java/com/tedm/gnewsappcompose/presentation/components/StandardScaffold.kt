f8cd1e23fba3dfe5d1ecfb997164a98180a9257a
Tadas <tadasgailevicius@yahoo.com>
2021-08-21 22:28:28 +0300

    Add StandardScaffold.kt Component

---
+++ app/src/main/java/com/tedm/gnewsappcompose/presentation/components/StandardScaffold.kt
@@ -0,0 +1,87 @@
+package com.tedm.gnewsappcompose.presentation.components
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.tedm.gnewsappcompose.R
+import com.tedm.gnewsappcompose.domain.models.BottomNavItem
+import com.tedm.gnewsappcompose.presentation.util.Screen
+
+@Composable
+fun StandardScaffold(
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    showBottomBar: Boolean = true,
+    showToolbar: Boolean = true,
+    showBackArrow: Boolean = true,
+    navActions: @Composable RowScope.() -> Unit = {},
+    toolbarTitle: String? = null,
+    bottomNavItems: List<BottomNavItem> = listOf(
+        BottomNavItem(
+            route = Screen.HomeScreen.route,
+            icon = Icons.Outlined.Home,
+            contentDescription = "Home"
+        ),
+        BottomNavItem(
+            route = Screen.NewsScreen.route,
+            icon = Icons.Outlined.Article,
+            contentDescription = "News"
+        ),
+        BottomNavItem(
+            route = Screen.SearchScreen.route,
+            icon = Icons.Outlined.Search,
+            contentDescription = "Search"
+        ),
+        BottomNavItem(
+            route = Screen.ProfileScreen.route,
+            icon = Icons.Outlined.Person,
+            contentDescription = "Profile"
+        ),
+        BottomNavItem(
+            route = Screen.MoreScreen.route,
+            icon = Icons.Outlined.More,
+            contentDescription = "More"
+        ),
+    ),
+    onFabClick: () -> Unit = {},
+    content: @Composable () -> Unit
+) {
+    Scaffold(
+        bottomBar = {
+            if (showBottomBar) {
+                BottomAppBar(
+                    modifier = Modifier.fillMaxWidth(),
+                    backgroundColor = MaterialTheme.colors.surface,
+                    cutoutShape = CircleShape,
+                    elevation = 5.dp
+                ) {
+                    BottomNavigation {
+                        bottomNavItems.forEachIndexed { i, bottomNavItem ->
+                            StandardBottomNavItem(
+                                icon = bottomNavItem.icon,
+                                contentDescription = bottomNavItem.contentDescription,
+                                selected = bottomNavItem.route == navController.currentDestination?.route,
+                                alertCount = bottomNavItem.alertCount,
+                                enabled = bottomNavItem.icon != null
+                            ) {
+                                if (navController.currentDestination?.route != bottomNavItem.route) {
+                                    navController.navigate(bottomNavItem.route)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        modifier = modifier
+    ) {
+        content()
+    }
+}
