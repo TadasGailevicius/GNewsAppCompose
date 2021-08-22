ad8d2762368322fa08a9d44e1103adee5c975b6e
Tadas <tadasgailevicius@yahoo.com>
2021-08-22 18:10:19 +0300

    Add NewsScreen.kt Screen

---
+++ app/src/main/java/com/tedm/gnewsappcompose/presentation/news/NewsScreen.kt
@@ -0,0 +1,19 @@
+package com.tedm.gnewsappcompose.presentation.news
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@Composable
+fun NewsScreen(navController: NavController) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "News screen")
+    }
+}
